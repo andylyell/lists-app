a6d4eadeb62e10d053ed5c83678fd4d540beb58b
andylyell <andy_lyell@hotmail.co.uk>
2019-06-05 22:13:13 +0100

    completed the styles for the homelist component and the logic behind this - needs a clean up

--- front-end/src/components/Home/HomeList/HomeList.js
+++ front-end/src/components/Home/HomeList/HomeList.js
@@ -3,26 +3,45 @@ import Button from '../../UI/Button/Button';
 
 function HomeList(props) {
 
-    const listItemsChecked = listItems => {
+    const checkedListItems = (listItems) => {
+
         let counter = 0;
         listItems.forEach(item => {
-            //check if listItems.isChecked is true or false
-            if(item.isChecked){
-                //if  it is true then add 1 to a counter
+            if(item.isChecked){ 
                 counter++ 
             }
         })
-        //at the end return the string array that compares checked items to length of whole array
-        return `${counter} / ${listItems.length}`;
+        return `${counter} / ${listItems.length}`; 
+    };
+
+    const checkListStyle = (listItems) => {
+
+        let counter = 0;
+        let itemStyles = '';
+
+        if(listItems.length !== 0){
+            listItems.forEach(item => {
+                if(item.isChecked){ 
+                    counter++ 
+                }
+                if(counter === listItems.length){
+                    itemStyles = ['homelist__list-items', 'homelist__list-items--complete'].join(' ');
+                } else {
+                    itemStyles = "homelist__list-items";    
+                }
+            })
+        } else {
+            itemStyles = "homelist__list-items";
+        }
+        
+        return itemStyles; 
     }
-
-
 
     return (
         <div className="homelist__container">
             <h4 className="homelist__list-title">{props.listName}</h4>
             <div className="homelist__info-group">
-                <p className="homelist__list-items">{listItemsChecked(props.listItems)}</p>
+                <p className={checkListStyle(props.listItems)}>{checkedListItems(props.listItems)}</p>
                 <Button clicked={console.log('button')} btnType="secondary" iconType="settings"/>
             </div>
         </div>
@@ -30,3 +49,52 @@ function HomeList(props) {
 }
 
 export default HomeList;
+
+
+// #region old code
+
+/*
+
+import React from 'react'
+import Button from '../../UI/Button/Button';
+
+function HomeList(props) {
+
+    let listItemsStyles = '';
+
+    const listItemsChecked = listItems => {
+        let counter = 0;
+        const itemsLength = parseInt(listItems.length); 
+
+        listItems.forEach(item => { //check if listItems.isChecked is true or false
+            if(item.isChecked){ //if  it is true then add 1 to a counter
+                counter++ 
+            }
+            if(counter === itemsLength && itemsLength !== 0){
+                listItemsStyles = ['homelist__list-items', 'homelist__list-items--complete'].join(' ');
+            } else {
+                listItemsStyles = "homelist__list-items";    
+            }
+        })
+        
+        return `${counter} / ${listItems.length}`; //at the end return the string array that compares checked items to length of whole array
+    }
+
+    // listComplete ? listItemsStyles = ['homelist__list-items', 'homelist__list-items--complete'].join(' ') : listItemsStyles = "homelist__list-items";
+
+    return (
+        <div className="homelist__container">
+            <h4 className="homelist__list-title">{props.listName}</h4>
+            <div className="homelist__info-group">
+                <p className={listItemsStyles}>{listItemsChecked(props.listItems)}</p>
+                <Button clicked={console.log('button')} btnType="secondary" iconType="settings"/>
+            </div>
+        </div>
+    )
+}
+
+export default HomeList;
+
+*/
+
+// #endregion
