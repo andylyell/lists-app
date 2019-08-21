df4b6b005e4ef5a032565de88fd1d6f8fe9fa4a5
andylyell <andy_lyell@hotmail.co.uk>
2019-08-21 09:01:57 +0100

    starting to think about the list and how that is rendered - added what I think the components will be

--- front-end/src/components/List/List.js
+++ front-end/src/components/List/List.js
@@ -6,9 +6,9 @@ const List = (props) => {
     return (
         <div>
             <div className="list__title">
-                {props.activeList.name}
+                <h1>{props.activeList.name}</h1>
                 <div className="list__control-group">
-                    <Button btnType="primary" iconType="add" text="Create Item"/>
+                    <Button btnType="primary" iconType="add" text="Add Item"/>
                     <Button clicked={console.log('button')} btnType="secondary" iconType="settings"/>
                 </div>
             </div>
