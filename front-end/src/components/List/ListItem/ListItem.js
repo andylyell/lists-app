fcf245d62dc505bcf65f42b8a3b792d7186cac9b
andylyell <andy_lyell@hotmail.co.uk>
2019-09-04 21:48:21 +0100

    added amount of list items and started working on the checkbox styling and functions

--- front-end/src/components/List/ListItem/ListItem.js
+++ front-end/src/components/List/ListItem/ListItem.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const ListItem = (props) => {
 
-    const { name } = props.itemDetail;
+    const { name, amount } = props.itemDetail;
 
     return (
         <div className="list-item">
             <div className="list-item__title">
+                <input type="checkbox" name="itemChecked" className="list-item__checkbox"/>
                 {name}
             </div>
             <div className="list-item__control-group">
-                <p>AMOUNT</p>
+                <div className="list-item__amount">x {amount}</div>   
                 <p>EDIT</p>
             </div>
         </div>
